1024e114fd1ab460b19a2780186cc842e05e28f2
John1Tang <1253703152@qq.com>
2020-04-14 22:43:39 +0800

    add two numbers

---
+++ leecode-0011/src/main/java/com/jt/algo/practice/Lc0445.java
@@ -0,0 +1,56 @@
+package com.jt.algo.practice;
+
+import java.util.Stack;
+
+/**
+ * 445. Add Two Numbers II
+ * You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Follow up:
+ * What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+ *
+ * Example:
+ *
+ * Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 8 -> 0 -> 7
+ *
+ */
+public class Lc0445 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        ListNode head = null;
+        int carry = 0;
+        while (!s1.isEmpty() || !s2.isEmpty() || carry > 0) {
+            int sum = carry;
+            sum += s1.isEmpty() ? 0 : s1.pop();
+            sum += s2.isEmpty() ? 0 : s2.pop();
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+            carry = sum / 10;
+        }
+        return head;
+    }
+}   
