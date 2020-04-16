b112b36b2aeafc85f75cab25e46f00bf34ab5ae6
John1Tang <1253703152@qq.com>
2020-04-16 23:06:49 +0800

    Merge Intervals

---
+++ src/main/java/com/jt/algo/practice/Lc0056.java
@@ -0,0 +1,39 @@
+package com.jt.algo.practice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 56. Merge Intervals
+ * Given a collection of intervals, merge all overlapping intervals.
+ *
+ * Example 1:
+ *
+ * Input: [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+ * Example 2:
+ *
+ * Input: [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ * NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+ *
+ */
+public class Lc0056 {
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, Comparator.comparingInt(v -> v[0]));
+
+        int[][] res = new int[intervals.length][2];
+        int idx = -1;
+        for (int[] interval: intervals) {
+            if (idx == -1 || interval[0] > res[idx][1]) {
+                res[++idx] = interval;
+            } else {
+                res[idx][1] = Math.max(res[idx][1], interval[1]);
+            }
+        }
+        return Arrays.copyOf(res, idx + 1);
+    }
+}
