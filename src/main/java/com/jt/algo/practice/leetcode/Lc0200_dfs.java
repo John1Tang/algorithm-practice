57ca464a9d03742b1d2783416740473bd4f9c433
John1Tang <1253703152@qq.com>
2020-04-20 22:50:43 +0800

    Number of Islands

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0200_dfs.java
@@ -0,0 +1,65 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 200. Number of Islands
+ * @author: john
+ * @created: 2020/04/20 21:09
+ *
+ *
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Example 1:
+ *
+ * Input:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ *
+ * Output: 1
+ * Example 2:
+ *
+ * Input:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ *
+ * Output: 3
+ *
+ */
+public class Lc0200_dfs {
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int numIslands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++numIslands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+        return numIslands;
+    }
+}
