dd09987b4ecb4ad01be5c30779894c7cf9df0116
John1Tang <1253703152@qq.com>
2020-05-05 22:55:22 +0800

    Two Sum

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0001.java
@@ -0,0 +1,36 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 1. Two Sum
+ * @author: john
+ * @created: 2020/05/05 22:54
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ *
+ */
+public class Lc0001 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum result");
+    }
+}
