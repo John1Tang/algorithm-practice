98cbcb70baa38cc8646fd31ecacef4f595352045
John1Tang <1253703152@qq.com>
2020-05-05 20:47:42 +0800

    Validate Binary Search Tree

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0098.java
@@ -0,0 +1,68 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @description: 98. Validate Binary Search Tree
+ * @author: john
+ * @created: 2020/05/05 20:42
+ * <p>
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * <p>
+ * Assume a BST is defined as follows:
+ * <p>
+ * The left subtree of a node contains only nodes with keys less than the node's key.
+ * The right subtree of a node contains only nodes with keys greater than the node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *  
+ * <p>
+ * Example 1:
+ * <p>
+ * 2
+ * / \
+ * 1   3
+ * <p>
+ * Input: [2,1,3]
+ * Output: true
+ * Example 2:
+ * <p>
+ * 5
+ * / \
+ * 1   4
+ *      / \
+ *     3   6
+ * <p>
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ */
+public class Lc0098 {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        double inOrder = -Double.MAX_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= inOrder) {
+                return false;
+            }
+            inOrder = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
