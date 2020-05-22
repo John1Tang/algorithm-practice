9072cbe7f020b3a44b91d6748e9b2ad010317508
John1Tang <1253703152@qq.com>
2020-05-22 23:19:10 +0800

    Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0105.java
@@ -0,0 +1,63 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @description: 105. Construct Binary Tree from Preorder and Inorder Traversal
+ * @author: john
+ * @created: 2020/05/22 23:16
+ * <p>
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ * <p>
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ * <p>
+ * For example, given
+ * <p>
+ * preorder = [3,9,20,15,7]
+ * inorder = [9,3,15,20,7]
+ * Return the following binary tree:
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ */
+public class Lc0105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[0]);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        int inorderIdx = 0;
+        for (int i = 1; i < preorder.length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIdx]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIdx]) {
+                    node = stack.pop();
+                    inorderIdx++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
