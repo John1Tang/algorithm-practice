cdefee84196a8f7e5f95242dc416ae7664baee24
John1Tang <1253703152@qq.com>
2020-06-17 22:34:00 +0800

    Longest Common Prefix; Serialize and Deserialize Binary Tree

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0014.java
@@ -0,0 +1,59 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 14. Longest Common Prefix
+ * @author: john
+ * @created: 2020/06/16 22:15
+ *
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ *
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ *
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * Note:
+ *
+ * All given inputs are in lowercase letters a-z.
+ */
+public class Lc0014 {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int minLength = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLength = Math.min(minLength, str.length());
+        }
+        int low = 0, high = minLength;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (isCommonPrefix(strs, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return strs[0].substring(0, low);
+    }
+
+    public boolean isCommonPrefix(String[] strs, int length) {
+        String str0 = strs[0].substring(0, length);
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            String str = strs[i];
+            for (int j = 0; j < length; j++) {
+                if (str0.charAt(j) != str.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
