55635480cdf3a5d5086da37ee95c4dd13655a5e8
John1Tang <1253703152@qq.com>
2020-06-28 23:20:11 +0800

    First Missing Positive; Minimum Size Subarray Sum

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0041.java
@@ -0,0 +1,44 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 41. First Missing Positive
+ * @author: john
+ * @created: 2020/06/28 23:18
+ *
+ * Given an unsorted integer array, find the smallest missing positive integer.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,0]
+ * Output: 3
+ * Example 2:
+ *
+ * Input: [3,4,-1,1]
+ * Output: 2
+ * Example 3:
+ *
+ * Input: [7,8,9,11,12]
+ * Output: 1
+ * Note:
+ *
+ * Your algorithm should run in O(n) time and uses constant extra space.
+ *
+ */
+public class Lc0041 {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0209.java
@@ -0,0 +1,40 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 209. Minimum Size Subarray Sum
+ * @author: john
+ * @created: 2020/06/28 23:19
+ *
+ * Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+ *
+ * Example: 
+ *
+ * Input: s = 7, nums = [2,3,1,2,4,3]
+ * Output: 2
+ * Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+ * Follow up:
+ * If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n). 
+ *
+ *
+ */
+public class Lc0209 {
+    public int minSubArrayLen(int s, int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += nums[end];
+            while (sum >= s) {
+                ans = Math.min(ans, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+            end++;
+        }
+        return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+}
