f1111f6c8215cfca80b3e0b2a28431dd6b1ad69f
John1Tang <1253703152@qq.com>
2020-07-12 23:49:43 +0800

    Best Time to Buy and Sell Stock with Cooldown

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0309.java
@@ -0,0 +1,41 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 309. Best Time to Buy and Sell Stock with Cooldown
+ * @author: john
+ * @created: 2020/07/12 23:48
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ *
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+ *
+ * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ * After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+ * Example:
+ *
+ * Input: [1,2,3,0,2]
+ * Output: 3
+ * Explanation: transactions = [buy, sell, cooldown, buy, sell]
+ *
+ *
+ */
+public class Lc0309 {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0){
+            return 0;
+        }
+        int n = prices.length;
+        int f0 = -prices[0];
+        int f1 = 0;
+        int f2 = 0;
+        for(int i = 1; i < n; ++i){
+            int newf0 = Math.max(f0, f2 - prices[i]);
+            int newf1 = f0 + prices[i];
+            int newf2 = Math.max(f1, f2);
+            f0 = newf0;
+            f1 = newf1;
+            f2 = newf2;
+        }
+        return Math.max(f1, f2);
+    }
+}
