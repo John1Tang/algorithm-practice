bc770659da830f1ec3c494a69e855ccfcb36029a
John1Tang <1253703152@qq.com>
2020-08-03 22:31:41 +0800

    Longest Increasing Path in a Matrix

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0329.java
@@ -0,0 +1,68 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 329. Longest Increasing Path in a Matrix
+ * @author: john
+ * @created: 2020/08/03 22:30
+ *
+ * Given an integer matrix, find the length of the longest increasing path.
+ *
+ * From each cell, you can either move to four directions: left, right, up or down. You may NOT move diagonally or move outside of the boundary (i.e. wrap-around is not allowed).
+ *
+ * Example 1:
+ *
+ * Input: nums =
+ * [
+ *   [9,9,4],
+ *   [6,6,8],
+ *   [2,1,1]
+ * ]
+ * Output: 4
+ * Explanation: The longest increasing path is [1, 2, 6, 9].
+ * Example 2:
+ *
+ * Input: nums =
+ * [
+ *   [3,4,5],
+ *   [3,2,6],
+ *   [2,2,1]
+ * ]
+ * Output: 4
+ * Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+ *
+ *
+ */
+public class Lc0329 {
+    public int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public int rows, columns;
+
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        rows = matrix.length;
+        columns = matrix[0].length;
+        int[][] memo = new int[rows][columns];
+        int ans = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                ans = Math.max(ans, dfs(matrix, i, j, memo));
+            }
+        }
+        return ans;
+    }
+
+    public int dfs(int[][] matrix, int row, int column, int[][] memo) {
+        if (memo[row][column] != 0) {
+            return memo[row][column];
+        }
+        ++memo[row][column];
+        for (int[] dir : dirs) {
+            int newRow = row + dir[0], newColumn = column + dir[1];
+            if (newRow >= 0 && newRow < rows && newColumn >= 0 && newColumn < columns && matrix[newRow][newColumn] > matrix[row][column]) {
+                memo[row][column] = Math.max(memo[row][column], dfs(matrix, newRow, newColumn, memo) + 1);
+            }
+        }
+        return memo[row][column];
+    }
+}
