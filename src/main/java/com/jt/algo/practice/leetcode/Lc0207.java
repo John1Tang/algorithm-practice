ed83e203d2a01be722eb837cf176899973e749a1
John1Tang <1253703152@qq.com>
2020-08-05 00:25:44 +0800

    Course Schedule

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0207.java
@@ -0,0 +1,79 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 207. Course Schedule
+ * @author: john
+ * @created: 2020/08/05 00:24
+ *
+ * There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+ *
+ * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+ *
+ * Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: numCourses = 2, prerequisites = [[1,0]]
+ * Output: true
+ * Explanation: There are a total of 2 courses to take.
+ *              To take course 1 you should have finished course 0. So it is possible.
+ * Example 2:
+ *
+ * Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+ * Output: false
+ * Explanation: There are a total of 2 courses to take.
+ *              To take course 1 you should have finished course 0, and to take course 0 you should
+ *              also have finished course 1. So it is impossible.
+ *  
+ *
+ * Constraints:
+ *
+ * The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+ * You may assume that there are no duplicate edges in the input prerequisites.
+ * 1 <= numCourses <= 10^5
+ *
+ *
+ */
+public class Lc0207 {
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        for (int i = 0; i < numCourses & valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    public void dfs(int u) {
+        visited[u] = 1;
+        for (int v : edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return ;
+                }
+            } else if(visited[v] == 1) {
+                valid = false;
+                return ;
+            }
+        }
+        visited[u] = 2;
+    }
+}
