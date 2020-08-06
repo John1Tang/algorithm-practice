4badcb8ae667939029867e0458fb1c69d5ee2ef9
John1Tang <1253703152@qq.com>
2020-08-06 10:37:41 +0800

    House Robber III

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0337.java
@@ -0,0 +1,63 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 337. House Robber III
+ * @author: john
+ * @created: 2020/08/06 10:33
+ * <p>
+ * The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+ * <p>
+ * Determine the maximum amount of money the thief can rob tonight without alerting the police.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,2,3,null,3,null,1]
+ * <p>
+ * 3
+ * / \
+ * 2   3
+ * \   \
+ * 3   1
+ * <p>
+ * Output: 7
+ * Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+ * Example 2:
+ * <p>
+ * Input: [3,4,5,1,3,null,1]
+ * <p>
+ *      3
+ * / \
+ * 4   5
+ * / \   \
+ * 1   3   1
+ * <p>
+ * Output: 9
+ * Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ */
+public class Lc0337 {
+    public int rob(TreeNode root) {
+        int[] rootStatus = dfs(root);
+        return Math.max(rootStatus[0], rootStatus[1]);
+    }
+
+    public int[] dfs(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+        int[] l = dfs(node.left);
+        int[] r = dfs(node.right);
+        int selected = node.val + l[1] + r[1];
+        int notSelected = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return new int[]{selected, notSelected};
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
