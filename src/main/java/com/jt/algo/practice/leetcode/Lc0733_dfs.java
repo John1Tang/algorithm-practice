a324ae97cf4c6aa1dca312dbd7a0bb64ebbd9fcb
John1Tang <1253703152@qq.com>
2020-08-16 22:37:22 +0800

    Flood Fill

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0733_dfs.java
@@ -0,0 +1,57 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 733. Flood Fill
+ * @author: john
+ * @created: 2020/08/16 22:35
+ *
+ * An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+ *
+ * Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+ *
+ * To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+ *
+ * At the end, return the modified image.
+ *
+ * Example 1:
+ * Input:
+ * image = [[1,1,1],[1,1,0],[1,0,1]]
+ * sr = 1, sc = 1, newColor = 2
+ * Output: [[2,2,2],[2,2,0],[2,0,1]]
+ * Explanation:
+ * From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected
+ * by a path of the same color as the starting pixel are colored with the new color.
+ * Note the bottom corner is not colored 2, because it is not 4-directionally connected
+ * to the starting pixel.
+ * Note:
+ *
+ * The length of image and image[0] will be in the range [1, 50].
+ * The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+ * The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+ *
+ *
+ */
+public class Lc0733_dfs {
+    int[] dx = {1, 0, 0, -1};
+    int[] dy = {0, 1, -1, 0};
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int currColor = image[sr][sc];
+        if (currColor != newColor) {
+            dfs(image, sr, sc, currColor, newColor);
+        }
+        return image;
+    }
+
+    public void dfs(int[][] image, int x, int y, int color, int newColor) {
+        if (image[x][y] == color) {
+            image[x][y] = newColor;
+            for (int i = 0; i < 4; i++) {
+                int mx = x + dx[i], my = y + dy[i];
+                if (mx >= 0 && mx < image.length && my >= 0 && my < image[0].length) {
+                    dfs (image, mx, my, color, newColor);
+                }
+            }
+        }
+    }
+}
