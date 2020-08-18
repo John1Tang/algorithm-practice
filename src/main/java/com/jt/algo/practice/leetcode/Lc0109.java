7bc9fa47cdd173e08b11c2968d8af00f1ad68ff2
John1Tang <1253703152@qq.com>
2020-08-19 05:31:52 +0800

    Convert Sorted List to Binary Search Tree

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0109.java
@@ -0,0 +1,107 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 109. Convert Sorted List to Binary Search Tree
+ * @author: john
+ * @created: 2020/08/19 05:28
+ * <p>
+ * Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ * <p>
+ * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ * <p>
+ *  
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: head = [-10,-3,0,5,9]
+ * Output: [0,-3,9,-10,null,5]
+ * Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+ * Example 2:
+ * <p>
+ * Input: head = []
+ * Output: []
+ * Example 3:
+ * <p>
+ * Input: head = [0]
+ * Output: [0]
+ * Example 4:
+ * <p>
+ * Input: head = [1,3]
+ * Output: [3,1]
+ *  
+ * <p>
+ * Constraints:
+ * <p>
+ * The numner of nodes in head is in the range [0, 2 * 10^4].
+ * -10^5 <= Node.val <= 10^5
+ */
+public class Lc0109 {
+    ListNode globalHead;
+
+    public TreeNode sortedListToBST(ListNode head) {
+        globalHead = head;
+        int length = getLength(head);
+        return buildTree(0, length - 1);
+    }
+
+    public int getLength(ListNode head) {
+        int ret = 0;
+        while (head != null) {
+            ++ret;
+            head = head.next;
+        }
+        return ret;
+    }
+
+    public TreeNode buildTree(int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left + 1) / 2;
+        TreeNode root = new TreeNode();
+        root.left = buildTree(left, mid - 1);
+        root.val = globalHead.val;
+        globalHead = globalHead.next;
+        root.right = buildTree(mid + 1, right);
+        return root;
+    }
+
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
