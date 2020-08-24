42e64a766513d58f625dd12381bb904d7dead36c
John1Tang <1253703152@qq.com>
2020-08-24 21:03:19 +0800

    Bitwise AND of Numbers Range

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0201.java
@@ -0,0 +1,29 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 201. Bitwise AND of Numbers Range
+ * @author: john
+ * @created: 2020/08/24 21:01
+ *
+ * Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+ *
+ * Example 1:
+ *
+ * Input: [5,7]
+ * Output: 4
+ * Example 2:
+ *
+ * Input: [0,1]
+ * Output: 0
+ *
+ *
+ */
+public class Lc0201 {
+    public int rangeBitwiseAnd(int m, int n) {
+        while (m < n) {
+            // Brian Kernighan algo: remove the rightest 1
+            n = n & (n - 1);
+        }
+        return n;
+    }
+}
