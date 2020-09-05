7d90b6ee590896a4fd3475b33569cbe4136674a0
John1Tang <1253703152@qq.com>
2020-09-05 21:20:07 +0800

    Permutation Sequence

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0060.java
@@ -0,0 +1,63 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @description: 60. Permutation Sequence
+ * @author: john
+ * @created: 2020/09/05 21:18
+ *
+ *The set [1,2,3,...,n] contains a total of n! unique permutations.
+ *
+ * By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+ *
+ * "123"
+ * "132"
+ * "213"
+ * "231"
+ * "312"
+ * "321"
+ * Given n and k, return the kth permutation sequence.
+ *
+ * Note:
+ *
+ * Given n will be between 1 and 9 inclusive.
+ * Given k will be between 1 and n! inclusive.
+ * Example 1:
+ *
+ * Input: n = 3, k = 3
+ * Output: "213"
+ * Example 2:
+ *
+ * Input: n = 4, k = 9
+ * Output: "2314"
+ *
+ *
+ */
+public class Lc0060 {
+    public String getPermutation(int n, int k) {
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            factorial[i] = factorial[i - 1] * i;
+        }
+
+        --k;
+        StringBuffer ans = new StringBuffer();
+        int[] valid = new int[n + 1];
+        Arrays.fill(valid, 1);
+        for (int i = 1; i <= n; ++i) {
+            int order = k / factorial[n - i] + 1;
+            for (int j = 1; j <= n; ++j) {
+                order -= valid[j];
+                if (order == 0) {
+                    ans.append(j);
+                    valid[j] = 0;
+                    break;
+                }
+            }
+            k %= factorial[n - i];
+        }
+        return ans.toString();
+    }
+}
