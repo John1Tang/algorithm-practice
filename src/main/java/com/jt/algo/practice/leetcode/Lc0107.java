e210b2cc9e29fc95811ab5540336dbb0a45a6f51
John1Tang <1253703152@qq.com>
2020-09-06 22:09:26 +0800

    Binary Tree Level Order Traversal II

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0107.java
@@ -0,0 +1,66 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @description: 107. Binary Tree Level Order Traversal II
+ * @author: john
+ * @created: 2020/09/06 22:05
+ * <p>
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+ * <p>
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its bottom-up level order traversal as:
+ * [
+ * [15,7],
+ * [9,20],
+ * [3]
+ * ]
+ */
+public class Lc0107 {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> levelOrder = new LinkedList<>();
+        if (root == null) {
+            return levelOrder;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                TreeNode left = node.left, right = node.right;
+                if (left != null) {
+                    queue.offer(left);
+                }
+                if (right != null) {
+                    queue.offer(right);
+                }
+            }
+            levelOrder.add(0, level);
+        }
+        return levelOrder;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
