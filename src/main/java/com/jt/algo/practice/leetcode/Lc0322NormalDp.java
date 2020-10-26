359dc0ba6070c4793b98bec2c50d8170b5f1a84f
John1Tang <1253703152@qq.com>
2020-10-27 00:05:19 +0800

    Coin Change; How Many Numbers Are Smaller Than the Current Number

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0322NormalDp.java
@@ -0,0 +1,75 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @description: 322. Coin Change
+ * @author: john
+ * @created: 2020/10/27 00:01
+ *
+ * You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+ *
+ * You may assume that you have an infinite number of each kind of coin.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: coins = [1,2,5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ * Example 2:
+ *
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * Example 3:
+ *
+ * Input: coins = [1], amount = 0
+ * Output: 0
+ * Example 4:
+ *
+ * Input: coins = [1], amount = 1
+ * Output: 1
+ * Example 5:
+ *
+ * Input: coins = [1], amount = 2
+ * Output: 2
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= coins.length <= 12
+ * 1 <= coins[i] <= 231 - 1
+ * 0 <= amount <= 104
+ *
+ *
+ */
+public class Lc0322NormalDp {
+    public int coinChange(int[] coins, int amount) {
+        if (amount < 1) {
+            return 0;
+        }
+        return coinChange(coins, amount, new int[amount]);
+    }
+
+    private int coinChange(int[] coins, int rem, int[] count) {
+        if (rem < 0) {
+            return -1;
+        }
+        if (rem == 0) {
+            return 0;
+        }
+        if (count[rem - 1] != 0) {
+            return count[rem - 1];
+        }
+        int min = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int res = coinChange(coins, rem - coin, count);
+            if (res >= 0 && res < min) {
+                min = 1 + res;
+            }
+        }
+        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+        return count[rem - 1];
+    }
+}
