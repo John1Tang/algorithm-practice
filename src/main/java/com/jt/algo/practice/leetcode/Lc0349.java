73dd67199234210511e41e1aca0be1986e5cde53
John1Tang <1253703152@qq.com>
2020-11-03 21:59:17 +0800

    Intersection of Two Arrays; Valid Mountain Array; 二叉树任务调度

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0349.java
@@ -0,0 +1,50 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @description: 349. Intersection of Two Arrays
+ * @author: john
+ * @created: 2020/11/03 18:50
+ *
+ * Given two arrays, write a function to compute their intersection.
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2]
+ * Example 2:
+ *
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [9,4]
+ * Note:
+ *
+ * Each element in the result must be unique.
+ * The result can be in any order.
+ *
+ *
+ */
+public class Lc0349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[length1 + length2];
+        int index = 0, index1 = 0, index2 = 0;
+        while (index1 < length1 && index2 < length2) {
+            int num1 = nums1[index1], num2 = nums2[index2];
+            if (num1 == num2) {
+                if (index == 0 || num1 != intersection[index - 1]) {
+                    intersection[index++] = num1;
+                }
+                index1++;
+                index2++;
+            } else if (num1 < num2) {
+                index1++;
+            } else {
+                index2++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+}
