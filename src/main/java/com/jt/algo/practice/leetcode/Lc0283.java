81362ba316ea35f3a6ab494a5ebedeb79ab4c5c2
John1Tang <1253703152@qq.com>
2020-11-19 23:12:37 +0800

    Gas Station; Move Zeroes; Queue Reconstruction by Height; Matrix Cells in Distance Order

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0283.java
@@ -0,0 +1,37 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 283. Move Zeroes
+ * @author: john
+ * @created: 2020/11/19 21:13
+ *
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ *
+ * Example:
+ *
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * Note:
+ *
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ *
+ */
+public class Lc0283 {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+}
