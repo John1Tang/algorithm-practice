ddd26fc37169ba83c8bd6f3db0c79f93445af80b
John1Tang <1253703152@qq.com>
2020-11-22 23:45:32 +0800

    Valid Anagram

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0242.java
@@ -0,0 +1,48 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 242. Valid Anagram
+ * @author: john
+ * @created: 2020/11/22 23:42
+ *
+ * Given two strings s and t , write a function to determine if t is an anagram of s.
+ *
+ * Example 1:
+ *
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * Note:
+ * You may assume the string contains only lowercase alphabets.
+ *
+ * Follow up:
+ * What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ *
+ *
+ */
+public class Lc0242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> table = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            table.put(ch, table.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            table.put(ch, table.getOrDefault(ch, 0) - 1);
+            if (table.get(ch) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
