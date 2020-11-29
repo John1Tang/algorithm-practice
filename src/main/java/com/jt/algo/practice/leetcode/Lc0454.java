6d7c7cc2a1bfad5f1e3aef9f7c5911f263eb1eb3
John1Tang <1253703152@qq.com>
2020-11-29 21:52:06 +0800

    4Sum II; Largest Perimeter Triangle

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0454.java
@@ -0,0 +1,50 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 454. 4Sum II
+ * @author: john
+ * @created: 2020/11/28 13:00
+ *
+ * Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+ *
+ * To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+ *
+ * Example:
+ *
+ * Input:
+ * A = [ 1, 2]
+ * B = [-2,-1]
+ * C = [-1, 2]
+ * D = [ 0, 2]
+ *
+ * Output:
+ * 2
+ *
+ * Explanation:
+ * The two tuples are:
+ * 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+ * 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+ *
+ */
+public class Lc0454 {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> countAB = new HashMap<>();
+        for (int u : A) {
+            for (int v : B) {
+                countAB.put(u + v, countAB.getOrDefault(u + v, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (int u : C) {
+            for (int v : D) {
+                if (countAB.containsKey(-u - v)) {
+                    ans += countAB.get(-u - v);
+                }
+            }
+        }
+        return ans;
+    }
+}
