c764c1449c1767a9fa34288e909d3e6f291eee82
John1Tang <1253703152@qq.com>
2020-12-20 23:43:19 +0800

    Remove Duplicate Letters

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0316.java
@@ -0,0 +1,58 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 316. Remove Duplicate Letters
+ * @author: john
+ * @created: 2020/12/20 23:42
+ *
+ * Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+ *
+ * Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: s = "bcabc"
+ * Output: "abc"
+ * Example 2:
+ *
+ * Input: s = "cbacdcbc"
+ * Output: "acdb"
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 104
+ * s consists of lowercase English letters.
+ *
+ *
+ */
+public class Lc0316 {
+    public String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+}
