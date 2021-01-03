7f8da4065a3547d68d9b5eea47dec3ce120a4866
John1Tang <1253703152@qq.com>
2021-01-03 19:29:32 +0800

    Partition List

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0086.java
@@ -0,0 +1,46 @@
+package com.jt.algo.practice.leetcode;
+
+/**
+ * @description: 86. Partition List
+ * @author: john
+ * @created: 2021/01/03 19:28
+ * <p>
+ * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+ * <p>
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ * <p>
+ * Example:
+ * <p>
+ * Input: head = 1->4->3->2->5->2, x = 3
+ * Output: 1->2->2->4->3->5
+ */
+public class Lc0086 {
+    public ListNode partition(ListNode head, int x) {
+        ListNode small = new ListNode(0);
+        ListNode smallHead = small;
+        ListNode large = new ListNode(0);
+        ListNode largeHead = large;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                large.next = head;
+                large = large.next;
+            }
+            head = head.next;
+        }
+        large.next = null;
+        small.next = largeHead.next;
+        return smallHead.next;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
