acbd3a22ee88298efc7dcb2a2ef08f1722e52d1c
John1Tang <1253703152@qq.com>
2021-03-15 00:31:01 +0800

    Design HashSet & HashMap

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0705.java
@@ -0,0 +1,102 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @description: 705. Design HashSet
+ * @author: john
+ * @created: 2021/03/14 23:50
+ *
+ * Design a HashSet without using any built-in hash table libraries.
+ *
+ * Implement MyHashSet class:
+ *
+ * void add(key) Inserts the value key into the HashSet.
+ * bool contains(key) Returns whether the value key exists in the HashSet or not.
+ * void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+ *  
+ *
+ * Example 1:
+ *
+ * Input
+ * ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+ * [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+ * Output
+ * [null, null, null, true, false, null, true, null, false]
+ *
+ * Explanation
+ * MyHashSet myHashSet = new MyHashSet();
+ * myHashSet.add(1);      // set = [1]
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(1); // return True
+ * myHashSet.contains(3); // return False, (not found)
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(2); // return True
+ * myHashSet.remove(2);   // set = [1]
+ * myHashSet.contains(2); // return False, (already removed)
+ *  
+ *
+ * Constraints:
+ *
+ * 0 <= key <= 106
+ * At most 104 calls will be made to add, remove, and contains.
+ *  
+ *
+ * Follow up: Could you solve the problem without using the built-in HashSet library?
+ *
+ *
+ */
+public class Lc0705 {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    /** Initialize your data structure here. */
+    public Lc0705() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; ++i) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[h].remove(element);
+                return;
+            }
+        }
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
