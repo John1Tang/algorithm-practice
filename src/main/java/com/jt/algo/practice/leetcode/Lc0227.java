fa0f6be2cf25d63f4bcbf8120c22d66f29efa3f1
John1Tang <1253703152@qq.com>
2021-03-15 20:12:34 +0800

    Basic Calculator II

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0227.java
@@ -0,0 +1,79 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @description: 227. Basic Calculator II
+ * @author: john
+ * @created: 2021/03/15 20:04
+ *
+ * Given a string s which represents an expression, evaluate this expression and return its value. 
+ *
+ * The integer division should truncate toward zero.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: s = "3+2*2"
+ * Output: 7
+ * Example 2:
+ *
+ * Input: s = " 3/2 "
+ * Output: 1
+ * Example 3:
+ *
+ * Input: s = " 3+5 / 2 "
+ * Output: 5
+ *  
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 3 * 105
+ * s consists of integers and operators ('+', '-', '*', '/') separated by some number of spaces.
+ * s represents a valid expression.
+ * All the integers in the expression are non-negative integers in the range [0, 231 - 1].
+ * The answer is guaranteed to fit in a 32-bit integer.
+ *
+ *
+ */
+public class Lc0227 {
+    public static int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        calculate("3+2*2");
+    }
+}
