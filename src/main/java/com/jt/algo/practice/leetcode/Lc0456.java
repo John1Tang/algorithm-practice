e320433950c450980d498ff89dfba8b0cdbbc15e
John1Tang <1253703152@qq.com>
2021-04-10 15:28:14 +0800

    Set Matrix Zeroes; Search in Rotated Sorted Array II; Find Minimum in Rotated Sorted Array; Find Minimum in Rotated Sorted Array II; 132 Pattern; Longest Common Subsequence

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0456.java
@@ -0,0 +1,136 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @description: 456. 132 Pattern
+ * @author: john
+ * @created: 2021/04/10 14:57
+ *
+ * Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+ *
+ * Return true if there is a 132 pattern in nums, otherwise, return false.
+ *
+ * Follow up: The O(n^2) is trivial, could you come up with the O(n logn) or the O(n) solution?
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ * Explanation: There is no 132 pattern in the sequence.
+ * Example 2:
+ *
+ * Input: nums = [3,1,4,2]
+ * Output: true
+ * Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
+ * Example 3:
+ *
+ * Input: nums = [-1,3,2,0]
+ * Output: true
+ * Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+ *  
+ *
+ * Constraints:
+ *
+ * n == nums.length
+ * 1 <= n <= 104
+ * -109 <= nums[i] <= 109
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/132-pattern
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Lc0456 {
+    public boolean find132patternStack(int[] nums) {
+        int n = nums.length;
+        Deque<Integer> candidateK = new LinkedList<>();
+        candidateK.push(nums[n - 1]);
+        int maxK = Integer.MIN_VALUE;
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (nums[i] < maxK) {
+                return true;
+            }
+            while (!candidateK.isEmpty() && nums[i] > candidateK.peek()) {
+                maxK = candidateK.pop();
+            }
+            if (nums[i] > maxK) {
+                candidateK.push(nums[i]);
+            }
+        }
+        return false;
+    }
+
+
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        List<Integer> candidateI = new ArrayList<>();
+        candidateI.add(nums[0]);
+        List<Integer> candidateJ = new ArrayList<>();
+        candidateJ.add(nums[0]);
+
+        for (int k = 1; k < n; ++k) {
+            int idxI = binarySearchFirst(candidateI, nums[k]);
+            int idxJ = binarySearchLast(candidateJ, nums[k]);
+            if (idxI >= 0 && idxJ >= 0) {
+                if (idxI <= idxJ) {
+                    return true;
+                }
+            }
+
+            if (nums[k] < candidateI.get(candidateI.size() - 1)) {
+                candidateI.add(nums[k]);
+                candidateJ.add(nums[k]);
+            } else if (nums[k] > candidateJ.get(candidateJ.size() - 1)) {
+                int lastI = candidateI.get(candidateI.size() - 1);
+                while (!candidateJ.isEmpty() && nums[k] > candidateJ.get(candidateJ.size() - 1)) {
+                    candidateI.remove(candidateI.size() - 1);
+                    candidateJ.remove(candidateJ.size() - 1);
+                }
+                candidateI.add(lastI);
+                candidateJ.add(nums[k]);
+            }
+        }
+
+        return false;
+    }
+
+    public int binarySearchFirst(List<Integer> candidate, int target) {
+        int low = 0, high = candidate.size() - 1;
+        if (candidate.get(high) >= target) {
+            return -1;
+        }
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            int num = candidate.get(mid);
+            if (num >= target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    public int binarySearchLast(List<Integer> candidate, int target) {
+        int low = 0, high = candidate.size() - 1;
+        if (candidate.get(low) <= target) {
+            return -1;
+        }
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            int num = candidate.get(mid);
+            if (num <= target) {
+                high = mid - 1;
+            } else {
+                low = mid;
+            }
+        }
+        return low;
+    }
+}
