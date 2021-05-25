c55ff44b9a1dcfb4ee083431c9fbfce56ce1d54d
John1Tang <1253703152@qq.com>
2021-05-25 22:12:18 +0800

    Top K Frequent Words; Chalkboard XOR Game; Make the XOR of All Segments Equal to Zero

---
+++ src/main/java/com/jt/algo/practice/leetcode/Lc0692.java
@@ -0,0 +1,58 @@
+package com.jt.algo.practice.leetcode;
+
+import java.util.*;
+
+/**
+ * @description: 692. Top K Frequent Words
+ * @author: john
+ * @created: 2021/05/25 21:38
+ *
+ *
+ * Given a non-empty list of words, return the k most frequent elements.
+ *
+ * Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
+ *
+ * Example 1:
+ * Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
+ * Output: ["i", "love"]
+ * Explanation: "i" and "love" are the two most frequent words.
+ *     Note that "i" comes before "love" due to a lower alphabetical order.
+ * Example 2:
+ * Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
+ * Output: ["the", "is", "sunny", "day"]
+ * Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
+ *     with the number of occurrence being 4, 3, 2 and 1 respectively.
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ * Input words contain only lowercase letters.
+ * Follow up:
+ * Try to solve it in O(n log k) time and O(n) extra space.
+ *
+ *
+ */
+public class Lc0692 {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> cnt = new HashMap<>();
+        for (String word : words) {
+            cnt.put(word, cnt.getOrDefault(word, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((entry1, entry2) -> {
+            return entry1.getValue() == entry2.getValue() ?
+                    entry2.getKey().compareTo(entry1.getKey()) : entry1.getValue() - entry2.getValue();
+        });
+
+        for (Map.Entry<String, Integer> entry : cnt.entrySet()) {
+            pq.offer(entry);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        List<String> ret = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ret.add(pq.poll().getKey());
+        }
+        Collections.reverse(ret);
+        return ret;
+    }
+}
